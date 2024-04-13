ffba864956b6f191208493f77d613970e2c70773
Rafael Bonach <rafa.bonach@gmail.com>
2024-04-13 22:34:06 +0000

    Adicionando a main para o Repl.it

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+
+-------------------- Informações --------------------
+
+  Sempre que for codar no repl.it, coloque o codigo 
+a baixo deste comentario. O repl.it não roda se não 
+tiver na main.
+
+"""
+
+print("Hello, Wolrd!")
