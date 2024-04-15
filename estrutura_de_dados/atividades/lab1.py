984e55cedb6e831ac23e240560700f1f58f0d288
Rafael Bonach <rafa.bonach@gmail.com>
2024-04-15 23:24:01 +0000

    Adicionando  as mudanças no código do Lab1

--- estrutura_de_dados/atividades/lab1.py
+++ estrutura_de_dados/atividades/lab1.py
@@ -1,4 +1,6 @@
+import lab1ComHashTable
 
+"""
 def procuraRepetida(arr):
   repeticoes = 0
   for i in range(len(arr)):
@@ -12,9 +14,33 @@ def procuraRepetida(arr):
         j+=1
     i +=1
   return repeticoes
+"""
+def contador(qtdRoupas, roupas):
+  hash = {}
+  for i in roupas:
+    hash[i] = 0
+
+  for i in roupas:
+    if (i in hash) == True:
+      hash[i] = hash[i]+1
+
+  repeticoes = 0
+  for i in hash:
+    repeticoes += (hash[i]-1)
+
+  print(repeticoes)
 
 
 qtdRoupas = int(input())
 roupas = input().split()
 
-print(procuraRepetida(roupas))
+
+"""
+  Exemplos:
+  5
+  A1 A2 A2 A1 B9
+  23
+  H6 D3 Y7 A1 Z7 M6 Z2 A1 L4 H8 J2 T3 X4 N9 X7 C3 W7 X4 A1 I6 J7 N9 I0
+  15
+  O2 N7 C8 I3 E5 L5 N5 Q6 U1 G9 T0 A8 Z8 K1 U4
+"""
