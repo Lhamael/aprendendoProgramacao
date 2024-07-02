21b6578548cd9376b281e8fc48bd3054faa98f69
RafaBonach <rafa.bonach@gmail.com>
2024-07-02 16:10:58 -0300

    Novas atividades

---
+++ estrutura_de_dados/aulas/problemaJosephus.py
@@ -0,0 +1,33 @@
+"""
+    Adicionar valores
+    Criar uma lista circular
+    Retirar valor da lista circular
+"""
+from pythonds.basic.queue import Queue
+
+
+class Seletor:
+    def __init__(self) -> None:
+        self.fifo = Queue()
+    
+    def acrescenta(self, nome):
+        self.fifo.enqueue(nome)
+
+    def retira(self):
+        return self.fifo.dequeue()
+
+    def rodar(self, qtd):
+        while self.fifo.size() > 1:
+            for i in range(qtd-1):
+                nome = self.retira()
+                self.acrescenta(nome)
+            self.retira()
+        return self.retira()
+    
+
+lista = Seletor()
+l = ["Josephus", "Timão", "Henrique", "Geronimus", "Cornelius"]
+for i in l:
+    lista.acrescenta(i)
+n = int(input())
+print(lista.rodar(n))
