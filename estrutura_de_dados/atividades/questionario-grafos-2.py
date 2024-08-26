6534bf5704e6bc688962837fbcdd125c487b528b
Rafael Bonach <rafa.bonach@gmail.com>
2024-08-26 14:24:03 +0000

    Estudando Grafos

--- estrutura_de_dados/atividades/questionario-grafos-2.py
+++ estrutura_de_dados/atividades/questionario-grafos-2.py
@@ -14,28 +14,52 @@ def buscaProfundidade(grafo, noAtual, fim, profundidade):
     grafo[noAtual][0] = "B"
     return -1
 """
+
 # A opcao mais viavel e utilizar o algoritmo de Dijkstra
-## Implemente aqui o algoritmo
+def algoritmoBuscaDijkastra(grafo, inicio, destino):
+  distancias = {node:float("INF") for node in grafo}  
+  distancias[inicio] = 0
+
+  listaPrioridade = [(0, inicio)]
+  while len(listaPrioridade) != 0:
+    distanciaAtual, noAtual = listaPrioridade.pop(0)
+
+    if distanciaAtual > distancias[noAtual]:
+      continue
+
+    for vizinho in grafo[noAtual]:
+      distanciaTotal = distanciaAtual + 1
+
+      if distanciaTotal < distancias[vizinho]:
+        distancias[vizinho] = distanciaTotal
+        listaPrioridade.append((distanciaTotal, vizinho))
+
+  distanciaPercorrida = distancias[destino]
+  if distanciaPercorrida == float("INF"):
+    return -1  
+  return distancias[destino]-1
 
 def criaGrafo(lista, origem, destino):
-    grafo = {}
-    for vertice in lista:
-        if vertice[0] not in grafo:
-            grafo[vertice[0]] = ["W"]
-        for aresta in vertice[2:]:
+  grafo = {}
+  for vertice in lista:
+    if vertice[0] not in grafo:
+      grafo[vertice[0]] = []
+    for aresta in vertice[2:]:
 
 #-------------------------------------------------------------#
-            if vertice[0] == origem and aresta == destino or vertice[0] == destino and aresta == origem:
-                    return 0
+      if vertice[0] == origem and aresta == destino or vertice[0] == destino and aresta == origem:
+              return 0
 #-------------------------------------------------------------#
 
-            if aresta not in grafo:
-                grafo[aresta] = ["W"]
+      if aresta not in grafo:
+        grafo[aresta] = []
 
-            grafo[vertice[0]].append(aresta)
-            grafo[aresta].append(vertice[0])
+      if aresta not in grafo[vertice[0]]:
+        grafo[vertice[0]].append(aresta)
+      if vertice[0] not in grafo[aresta]:
+        grafo[aresta].append(vertice[0])
 
-    return buscaProfundidade(grafo, origem, destino, 0)
+  return algoritmoBuscaDijkastra(grafo, origem, destino)
 
 
 
--- estrutura_de_dados/atividades/questionario-grafos-3.py
+++ estrutura_de_dados/atividades/questionario-grafos-3.py
@@ -1,53 +1,56 @@
-#from pythonds import Queue
-
 def lista_adjacencia(dadosVizinhanca):
-    vizinhos_adj = {}
-    for vizinhos in dadosVizinhanca:
-        if vizinhos[0] not in vizinhos_adj:
-            vizinhos_adj[vizinhos[0]] = {}
-        for i in range(2, len(vizinhos[2:])+1, 2):
-            if vizinhos[i+1] not in vizinhos_adj:
-                vizinhos_adj[vizinhos[i+1]] = {}
-            vizinhos_adj[vizinhos[0]][vizinhos[i+1]] = vizinhos[i]
-            vizinhos_adj[vizinhos[i+1]][vizinhos[0]] = vizinhos[i]
-    return vizinhos_adj
+  vizinhos_adj = {}
+  for vizinhos in dadosVizinhanca:
+    if vizinhos[0] not in vizinhos_adj:
+      vizinhos_adj[vizinhos[0]] = {}
+    for i in range(2, len(vizinhos[2:])+1, 2):
+      if vizinhos[i+1] not in vizinhos_adj:
+        vizinhos_adj[vizinhos[i+1]] = {}
+      vizinhos_adj[vizinhos[0]][vizinhos[i+1]] = vizinhos[i]
+      vizinhos_adj[vizinhos[i+1]][vizinhos[0]] = vizinhos[i]
+  return vizinhos_adj
 
 def algoritmo_de_Dijkstra(vizinhos_adj, inicio):
-    distancia = {no:[float('inf'), 1] for no in vizinhos_adj}
-    distancia[inicio][0] = 0
+  distancia = {no:[float('inf'), 1] for no in vizinhos_adj}
+  distancia[inicio][0] = 0
 
-    lista_prioridade = [(0, inicio)]
+  lista_prioridade = [(0, inicio)]
 
-    while len(lista_prioridade) != 0:
-        distancia_atual, no_atual = lista_prioridade.pop(0)
+  while len(lista_prioridade) != 0:
+    distancia_atual, no_atual = lista_prioridade.pop(0)
 
-        if distancia_atual > distancia[no_atual][0]:
-            continue
+    if distancia_atual > distancia[no_atual][0]:
+      continue
 
-        for vizinho, peso in vizinhos_adj[no_atual].items():
-            distancia_total = distancia_atual + peso
+    for vizinho, peso in vizinhos_adj[no_atual].items():
+      distancia_total = distancia_atual + peso
 
-            if distancia_total < distancia[vizinho][0]:
-                distancia[vizinho][0] = distancia_total
-                distancia[vizinho][1] = no_atual
-                lista_prioridade.append((distancia_total, vizinho))
+      if distancia_total < distancia[vizinho][0]:
+        distancia[vizinho][0] = distancia_total
+        distancia[vizinho][1] = no_atual
+        lista_prioridade.append((distancia_total, vizinho))
 
-    return distancia
+  return distancia
 
+"""
 def distancia_verdadeira(distancias, residencia):
-    if distancias[residencia][1] == 1:
-        return distancias[residencia][0]
-    return distancias[residencia][0] - distancia_verdadeira(distancias, distancias[residencia][1])
-
+  if distancias[residencia][1] == 1:
+    return distancias[residencia][0]
+  return distancias[residencia][0] - distancia_verdadeira(distancias, distancias[residencia][1])
+"""
 
 def sistemaTubulucacao(dadosVizinhanca):
-    vizinhos_adj = lista_adjacencia(dadosVizinhanca)
-    inicio = 1
-    distancias = algoritmo_de_Dijkstra(vizinhos_adj, inicio)
-    soma = 0
-    for residencia in distancias:
-        soma += distancia_verdadeira(distancias, residencia)
-    print(f"R$ {soma*3.14}")
+  vizinhos_adj = lista_adjacencia(dadosVizinhanca)
+  inicio = 1
+  distancias = algoritmo_de_Dijkstra(vizinhos_adj, inicio)
+  soma = 0
+  for residencia in distancias:
+    if distancias[residencia][1] == 1:
+      soma += distancias[residencia][0]
+    else:
+      temp = distancias[residencia][1]
+      soma += distancias[residencia][0] - distancias[temp][0]
+  print(f"R$ {soma*3.14}")
 
 
 
@@ -57,5 +60,5 @@ n = int(input())
 j = n
 dados = []
 for _ in range(n):
-    dados.append(list(map(int, input().split())))
+  dados.append(list(map(int, input().split())))
 sistemaTubulucacao(dados)
